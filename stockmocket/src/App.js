dfec1bff6b82ca7681f5bf4b2f87165b6f3d91e9
Jsze5341 <33404576+Jsze5341@users.noreply.github.com>
2021-09-20 18:06:17 -0400

    Add files via upload

--- stockmocket/src/App.js
+++ stockmocket/src/App.js
@@ -1,19 +1,25 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
 import Home from './components/pages/Home';
+import Login from './components/pages/Login_and_Register/Login'
+import Register from './components/pages/Login_and_Register/Register'
 
 
 function App() {
   return (
-    <React.Fragment>
+      <React.Fragment>
       <Router>
       <Navbar />
       <Switch>
-      <Route path='/' exact component={Home}/>
+             <Route path='/' exact component={Home} />
+             <Route path='/login' exact component={Login} />
+             <Route path='/register' exact component={Register} />
+
       </Switch>
       </Router>
+
     </React.Fragment>
   );
 }
