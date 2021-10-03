e5687579194bde9b85cbb2860f4dce2be4bf0cc5
ddxx123 <33131715+ddxx123@users.noreply.github.com>
2021-10-02 22:20:30 -0400

    Add files via upload
    I used a new lib 
    npm install react-bootstrap@next bootstrap@5.1.1

---
+++ pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../App.css';
+import HeroSection from '../HeroSection';
+import Reasons from '../Reasons';
+
+
+function Home () {
+    return (
+        <React.Fragment>
+            <HeroSection />
+            <Reasons />
+        </React.Fragment>
+    )
+}
+
+export default Home
---
+++ pages/StockRow.js
@@ -0,0 +1,51 @@
+import React, {Component} from "react";
+import iex from './iexapitoken.js'
+import './MockData.css';
+
+var latestime;
+var d ;
+var t;
+class StockRow extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+           data: {},
+        }
+    }
+componentDidMount() {
+    const url = `${iex.base_url}/stock/${this.props.ticker}/quote/?&token=${iex.api_token}`
+
+    fetch(url).then((Response) => Response.json()).then((data) => {
+        console.log(data)
+       latestime = (data.latestUpdate) 
+       t = new Date(latestime).toLocaleTimeString("en-US")
+       d = new Date(latestime).toLocaleDateString("en-US")
+        this.setState({
+           data: data
+           
+        })
+    })
+   
+}
+
+render (){
+    return (
+            <tr className = "tabledesign">
+               
+                <td className = "cellcolor"> {this.props.ticker} </td>
+                <td className = "companyname" > {this.state.data.companyName} </td>
+                <td className = "numbers"> $  {this.state.data.latestPrice} </td>
+                <td  className = "numbers" > $ {this.state.data.open} </td>
+                <td   className = "numbers"> $ {this.state.data.close} </td>
+                <td  className = "numbers"  style= {{color: Math.sign(this.state.data.changePercent) == -1 ? "red" : "green"}}> {this.state.data.changePercent} </td>
+                <td  className = "numbers"> {t + " " + d} </td>
+                <div> Chart??   </div>
+            </tr>
+
+    )
+}
+
+}
+
+export default StockRow;
