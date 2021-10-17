10832027efe3217df4106a75ff90873b1e142cc3
ddxx123 <33131715+ddxx123@users.noreply.github.com>
2021-10-17 10:58:27 -0400

    Add files via upload

--- stockmocket/src/components/pages/usermarketpage.js
+++ stockmocket/src/components/pages/usermarketpage.js
@@ -5,7 +5,7 @@ import "./usermarketpage.css"
 
 
 var user = "brian";
-var currentbal = "7564"
+var currentbal = parseFloat("1540.547892")
 var key1 ="tsla"
 var key = "";
 var logo1 = '';
@@ -18,7 +18,7 @@ class usermarketpage extends Component{
     constructor(props) {
         super(props);
         this.state = {
-           data: {}, logo: [], info:{}, value: ''
+           data: {}, logo: [], info:{}, value: 'tsla'
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -62,8 +62,8 @@ render() {
     return(
         <div className="uncontainer">
             <div className ="usergreeting">
-                <h1> Hello {user} </h1>
-                <h1> Current Balance is $ {currentbal}</h1>
+                <h1 className = "namedesc"> Hello {user} </h1>
+                <h1> Current Balance is $ {currentbal.toFixed(2)}</h1>
             </div> 
             <div className="searchbar">
             <form onSubmit={this.handleSubmit}>
@@ -81,24 +81,53 @@ render() {
                         <img src={s} />
                 </div>
                 <div className="stockinfo">
-                    Title {this.state.data.companyName}
-                    <br />
-                    price  {this.state.data.high} ### ###
-                    <br /> 
-                    ### ### ###
-                    <br /> 
-                    ### ### ###
+                    Company Name: {this.state.data.companyName}
+                   <div className="tableinfo">
+                   <table class="wp-table">
+                         <tr>
+                    <th className = "tablenamestyling">Industry :  <p1 className ="companyname">  {this.state.info.industry} </p1></th>
+                         <th className = "tablenamestyling">CEO :  <p1 className ="companyname"> {this.state.info.CEO} </p1></th>
+                        <th className = "tablenamestyling" >Sector : <p1 className ="companyname">  {this.state.info.sector} </p1> </th>
+                        </tr>
+                     <tr>
+                 <td className = "tablenamestyling">Price  $ <p1 className ="numbers"> {this.state.data.latestPrice} </p1> </td>
+                <td className = "tablenamestyling" >High $  <p1 className ="numbers">{this.state.data.high} </p1> </td>
+                    <td className = "tablenamestyling" >Low $  <p1 className ="numbers"> {this.state.data.low} </p1></td>
+                </tr>
+                 <tr>
+                     <td className = "tablenamestyling" >Volume : <p1 className ="numbers">{this.state.data.volume} </p1> </td>
+                      <td className = "tablenamestyling" > Week-52 High $ <p1 className ="numbers">{this.state.data.week52High} </p1> </td>
+                    <td className = "tablenamestyling" >Week-52 Low $ <p1 className ="numbers">{this.state.data.week52Low} </p1></td>
+                 </tr>
+                     <tr>
+                 <td className = "tablenamestyling" > MarketCap : <p1 className ="numbers">{this.state.data.marketCap} </p1> </td>
+                 <td className = "tablenamestyling"  >  Daily Change  $ <p1  style= {{color: Math.sign(this.state.data.change) == -1 ? "red" : "green"}}>  <p1 className ="numbers"> {this.state.data.change} </p1></p1> </td>
+                    <td className = "tablenamestyling" >YTD-Change : <p1  style= {{color: Math.sign(this.state.data.ytdChange) == -1 ? "red" : "green"}}> <p1 className ="numbers">{this.state.data.ytdChange} % </p1> </p1></td>
+    </tr>
+  </table>
+                   </div>
                 </div>
                 <div className="stockbutton">
-
+                        <p1> buy button underworks</p1>
                 </div>
                 </div>
                 <div className="stockdescription">
-                    <h1>  
+                <div className ='maxstockdescription'> 
+                <h1>"
+                    <p1 className ="descfont">  
                         {this.state.info.description}
-                    </h1>
+                        
+                    </p1>
+                   " </h1>
+                    </div>
+                   
                 </div>
+                <div className = "newcycle">
+                    <h1> NEW CYCLE UNDERWORKS</h1>
+                        <h1>  homas Edward Patrick Brady Jr. is an American football quarterback for the Tampa Bay Buccaneers of the National Football League. He spent his first 20 seasons with the New England Patriots, where he was a central contributor to the franchise's dynasty from 2001 to 201 </h1>
+                        </div>
            </div>
+           
         </div>
     )
 }
