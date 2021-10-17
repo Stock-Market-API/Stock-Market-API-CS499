7904938aaeecfad8d230ac3298cf901ea3c76060
Jsze5341 <33404576+Jsze5341@users.noreply.github.com>
2021-10-17 17:43:52 -0400

    Update App.js

--- stockmocket/src/App.js
+++ stockmocket/src/App.js
@@ -1,23 +1,36 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Home from './components/pages/Home';
 import Login from './components/pages/Login_and_Register/Login'
 import Register from './components/pages/Login_and_Register/Register'
+import Aboutus from './components/pages/Aboutus'
+import MarketData from './components/pages/MarketData'
+import Footer from './components/Footer';
+import ProfilePage from './components/pages/ProfilePage';
 
+const Parse = require('parse/node');
+const PARSE_APPLICATION_ID = 'PkKseo3f5GALlC6FbYTPof2xofIOOOqxsfeIJ2Lx';
+const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
+const PARSE_JAVASCRIPT_KEY = '2FokkUXZSKpMu65pSht3JHfeaGUKcwmbg4E5ISJ3';
+Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = PARSE_HOST_URL;
 
-function App() {
-  return (
+function App(props) {
+    return (
       <React.Fragment>
       <Router>
-      <Navbar />
+         <Navbar />
       <Switch>
              <Route path='/' exact component={Home} />
              <Route path='/login' exact component={Login} />
              <Route path='/register' exact component={Register} />
-
+             <Route path='/aboutus' exact component={Aboutus} />
+             <Route path='/market' exact component={MarketData} />
+             <Route path='/profile' exact component={ProfilePage} />
       </Switch>
+         <Footer />
       </Router>
 
     </React.Fragment>
