aa5669cc42495e9b4460a5fc92a327f66b89400f
Jsze5341 <33404576+Jsze5341@users.noreply.github.com>
2021-10-20 23:07:23 -0400

    Profile page stock and account value display

--- stockmocket/src/components/ProfilePage.js
+++ stockmocket/src/components/ProfilePage.js
@@ -1,26 +1,112 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import iex from './iexapitoken.js'
+import StockRow from "./StockRow.js"
 import "./ProfilePage.css";
+import "./MockData.css";
 
 const Parse = require('parse/node');
 
 function ProfilePage() {
     const [balance, setBalance] = useState(null);
     const [balanceDisplay, setbalanceDisplay] = useState(0);
+    const [stocks, setStocks] = useState([]);
+    const [stockPrice, setstockPrice] = useState(0);
+    const [shares, setShares] = useState(0);
+    const [stocksLength, setstocksLength] = useState(0);
 
     async function getUserBalance() {
-        const currentUser = await Parse.User.current();
-        setbalanceDisplay(currentUser.get('balance'));
+        try {
+            const currentUser = await Parse.User.current();
+            setbalanceDisplay(currentUser.get('balance'));
+        }
+        catch (err) {
+            //alert("Not logged in");
+        }
     }
 
-    window.addEventListener('submit', getUserBalance);
+    getUserBalance();
 
+    //Saves user balance to backend
     async function setUserBalance(event) {
         event.preventDefault();
         var intbalance = parseFloat(balance); 
+
         const currentUser = await Parse.User.current();
 
         currentUser.set('balance', intbalance);
         currentUser.save();
+    }
+
+    window.addEventListener('submit', getUserBalance);
+    window.addEventListener('load', getUserBalance);
+    window.addEventListener('submit', setUserBalance);
+
+    //Gets all of currently logged in user's stock data
+    async function getUserStocks() {
+        var stockName = [];
+        var AveragePrice = [];
+        var sharesBought = [];
+        var count = 0;
+
+        //Get username
+        const currentUser = await Parse.User.current();
+        const OwnerQuery = new Parse.Query('User');
+        OwnerQuery.equalTo('username', currentUser.get('username'));
+        const Owner = await OwnerQuery.first();
+
+        //Get all owned stocks
+        const portfolioQuery = new Parse.Query('Portfolio');
+        portfolioQuery.equalTo('stockOwner', Owner);
+        let queryResults = await portfolioQuery.find();
+
+        //Append stock data to be set to corresponding states
+        for (let result of queryResults) {
+            stockName.push(result.get('stockName'));
+            AveragePrice.push(result.get('AveragePrice'));
+            sharesBought.push(result.get('sharesBought'));
+            count++;
+        }
+
+        setStocks(stockName);
+        setstockPrice(AveragePrice);
+        setShares(sharesBought);
+        setstocksLength(count);
+
+    }
+
+    useEffect(() => {
+        window.addEventListener('load', getUserStocks());
+    }, []);
+
+    function stockDisplay() {
+        var profileStocks = [];
+
+        for (const stock of stocks) {
+            console.log("stock to push: ", stock);
+            profileStocks.push(<StockRow ticker={stock} />)
+        }
+        return profileStocks;
+    }
+
+    //Account Value = Total Stock Values + Balance
+    function accountvalueDisplay() {
+        var accountValue = 0;
+        var values = [];
+
+        if (stocksLength != 0) {
+            for (var i = 0; i < stocksLength; i++)
+                values.push(stockPrice[i] * shares[i]);
+
+            for (var i = 0; i < stocksLength; i++)
+                accountValue += values[i];
+
+            accountValue += balanceDisplay;
+            return accountValue;
+        }
+
+        else {
+            return 0;
+        }
     }
 
     return (
@@ -29,7 +115,7 @@ function ProfilePage() {
                 Assets
                 <div> <br/> </div>
                 <div className="account-value">
-                    Your Account Value: {/*will be stock values + cash balance*/}
+                    Your Account Value: ${accountvalueDisplay()}
                 </div>
                 <div className="cash-balance">
                     Cash Balance: ${balanceDisplay}
@@ -48,9 +134,29 @@ function ProfilePage() {
                     </form>
                 </div>
             </div>
-            <div className="stock-display">
-                stocks here
+            <tbody className="stock-table">
+                <div className="container">
+                    <div className="titledesign">  <h1> Your Stocks </h1></div>
+                    <table className="table">
+                        <thead>
+                            <tr className="chartdesign">
+
+                                <th className="publicsans"> TICKER </th>
+                                <th className="publicsans"> NAME</th>
+                                <th className="publicsans"> PRICE </th>
+                                <th className="publicsans"> OPEN </th>
+                                <th className="publicsans"> CLOSE</th>
+                                <th className="publicsans"> % CHANGE</th>
+                                <th className="publicsans"> LAST UPDATE </th>
+
+                            </tr>
+                        </thead>
+                        <tbody className="tabledesign">
+                            {stockDisplay()}
+                        </tbody>
+                    </table>
                 </div>
+            </tbody>
         </div>
 
     );
