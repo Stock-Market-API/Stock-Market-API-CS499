f7b089943a049b2bd12c93a01f3bb412d58f8a96
Jsze5341 <33404576+Jsze5341@users.noreply.github.com>
2021-11-03 20:42:08 -0400

    Navbar await optimized

--- stockmocket/src/components/Navbar.js
+++ stockmocket/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import LogoutButton from './LogoutButton';
@@ -31,8 +31,7 @@ function Navbar(props) {
     window.addEventListener('resize', showButton);
 
     const changeBackground = (event) => {
-        if (window.location.pathname == '/' || window.location.pathname == '/login' || window.location.pathname == '/register')
-        {
+        if (window.location.pathname == '/' || window.location.pathname == '/login' || window.location.pathname == '/register') {
             if (window.location.pathname.includes('/') && window.scrollY >= 80) {
                 setNavbar(true);
                 setButtonStyle(false);
@@ -57,9 +56,8 @@ function Navbar(props) {
 
     //Checks if a user is logged in or not
     //Sets loggedIn state to true if user is found
-    async function loginStatus() {
+    const loginStatus = useCallback(async () => {
         const currentUser = await Parse.User.current();
-        console.log(currentUser);
 
         if (currentUser !== null) {
             setloggedIn(true);
@@ -67,14 +65,15 @@ function Navbar(props) {
         else {
             setloggedIn(false);
         }
-    }
+    },[navbar])
 
-    loginStatus();
+    useEffect(() => {
+        loginStatus();
+    }, [loginStatus]);
 
     function loginButton() {
         return button && <Button link='/login' buttonStyle={buttonStyle ? 'btn--outline' : 'btn--primary'} >Login / Signup</Button>;
     }
-
 
     //Changes button display to either login or logout 
     //depending if user is logged in or not
